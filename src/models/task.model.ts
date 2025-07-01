c3f7f5859b44d1be5f8bd8080c72aa616f2dc133
senay808 <senay.bihon@aastustudent.edu.et>
2025-07-01 18:16:38 +0300

    Refactor TaskSchema

--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -3,10 +3,13 @@ import { ITask } from "../interfaces/task.interface";
 
 export interface ITaskDocument extends ITask, Document {}
 
-const TaskSchema: Schema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  completed: { type: Boolean, default: false },
-});
+const TaskSchema: Schema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    completed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model<ITaskDocument>("Task", TaskSchema);
